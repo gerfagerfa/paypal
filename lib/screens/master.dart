424add8ad985cff8caa1eca4b176c33dc5bcae19
Germán Fajardo Sánchez <gerfagerfa@gmail.com>
2020-09-08 17:43:31 -0300

    bottom bar finished

--- lib/screens/master.dart
+++ lib/screens/master.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paypal/shared/bottom_navigation_item.dart';
 
 class Master extends StatefulWidget {
   @override
@@ -8,8 +9,67 @@ class Master extends StatefulWidget {
 class _MasterState extends State<Master> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: Container(
+        height: 80,
+        color: Colors.white,
+        child: Column(
+          children: [
+
+            Container(
+              height: 2,
+              color: Colors.grey[300],
+            ),
+
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                  BottomNavigationItem(
+                    imageUrl: "assets/icons/tile.png",
+                    text: "Dashboard",
+                    selected: true,
+                    onPress: () {
+
+                    },
+                  ),
+
+                  BottomNavigationItem(
+                    imageUrl: "assets/icons/send_money.png",
+                    text: "Send",
+                    selected: false,
+                    onPress: () {
+                      
+                    },
+                  ),
+
+                  BottomNavigationItem(
+                    imageUrl: "assets/icons/request_money.png",
+                    text: "Request",
+                    selected: false,
+                    onPress: () {
+                      
+                    },
+                  ),
+
+                  BottomNavigationItem(
+                    imageUrl: "assets/icons/settings.png",
+                    text: "Settings",
+                    selected: false,
+                    onPress: () {
+                      
+                    },
+                  ),
+
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/shared/bottom_navigation_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:paypal/utils/constants.dart';
+
+class BottomNavigationItem extends StatelessWidget {
+
+  final String imageUrl;
+  final String text;
+  final bool selected;
+  final Function onPress;
+
+  BottomNavigationItem({@required this.imageUrl, @required this.text, @required this.selected, this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        SizedBox(
+          height: 24,
+          width: 24,
+          child: Image.asset(
+            imageUrl,
+            color: selected ? kPrimaryColor : Colors.grey[400],
+          ),
+        ),
+
+        SizedBox(
+          height: 12,
+        ),
+
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+            color: selected ? kPrimaryColor : Colors.grey[400],
+          ),
+        )
+
+      ],
+    );
+  }
+}
